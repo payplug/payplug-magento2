083df001bbaba48fd7c482ffe20f069b44d4a9ec
sophie-mulard <smulard@gmail.com>
2019-11-19 11:27:37 +0100

    Fix payment method availability for 0.99€ orders

--- Helper/Transaction/AbstractBuilder.php
+++ Helper/Transaction/AbstractBuilder.php
@@ -87,7 +87,7 @@ abstract class AbstractBuilder extends AbstractHelper
     public function buildAmountData($order)
     {
         $paymentTab = [
-            'amount' => (int) ($order->getGrandTotalAmount() * 100),
+            'amount' => (int) (round($order->getGrandTotalAmount(), 2) * 100),
         ];
 
         return $paymentTab;
