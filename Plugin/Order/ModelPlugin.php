13a487b0b28ea66c8caebb1e6ad7cade7fe9b8bf
sophie-mulard <smulard@gmail.com>
2021-10-29 09:48:50 +0200

    #MAG-139 Allow order cancellation

---
+++ Plugin/Order/ModelPlugin.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Payplug\Payments\Plugin\Order;
+
+use Magento\Sales\Model\Order;
+use Payplug\Payments\Helper\Data;
+
+class ModelPlugin
+{
+    /**
+     * @var Data
+     */
+    private $helper;
+
+    /**
+     * @param Data $helper
+     */
+    public function __construct(Data $helper)
+    {
+        $this->helper = $helper;
+    }
+
+    /**
+     * @param Order $subject
+     * @param bool  $result
+     *
+     * @return bool
+     */
+    public function afterCanCancel(Order $subject, $result): bool
+    {
+        return $result || $this->helper->canForceOrderCancel($subject);
+    }
+
+    /**
+     * @param Order $subject
+     *
+     * @return null
+     */
+    public function beforeCancel(Order $subject)
+    {
+        $this->helper->forceOrderCancel($subject);
+
+        return null;
+    }
+}
