621bfb2018a4d4907c9b626f4cb2e7459231bf47
Aurelie Lopez <alopez@payplug.com>
2022-06-09 10:55:35 +0200

    change version

--- Helper/Config.php
+++ Helper/Config.php
@@ -20,7 +20,7 @@ class Config extends AbstractHelper
     public const PAYMENT_PAGE_REDIRECT = 'redirect';
     public const PAYMENT_PAGE_EMBEDDED = 'embedded';
 
-    public const MODULE_VERSION = '1.21.1';
+    public const MODULE_VERSION = '1.21.2';
 
     /**
      * @var WriterInterface
