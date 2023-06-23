20ae6a60bf2a46daae39d23e5383ea9835787ec3
Edouard Lacan <elacan@payplug.com>
2023-06-23 12:50:34 +0200

    Rollback module version to 1.24.1

--- Helper/Config.php
+++ Helper/Config.php
@@ -25,7 +25,7 @@ class Config extends AbstractHelper
     public const PAYMENT_PAGE_INTEGRATED = 'integrated';
     public const PAYMENT_PAGE_MANUAL = 'manual';
 
-    public const MODULE_VERSION = '1.25.0';
+    public const MODULE_VERSION = '1.24.1';
 
     /**
      * @var WriterInterface
