bcfc9331629fa1f0d0eef979f26adb24dd246e97
sophie-mulard <smulard@gmail.com>
2023-08-31 11:32:20 +0200

    MAG-218 Update code version

--- Helper/Config.php
+++ Helper/Config.php
@@ -25,7 +25,7 @@ class Config extends AbstractHelper
     public const PAYMENT_PAGE_EMBEDDED = 'embedded';
     public const PAYMENT_PAGE_INTEGRATED = 'integrated';
 
-    public const MODULE_VERSION = '1.26.0';
+    public const MODULE_VERSION = '1.27.0';
 
     /**
      * @var WriterInterface
