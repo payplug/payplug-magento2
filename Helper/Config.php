cab1669dc8ce6b2b216b2e86ad6142d9b55870b9
Manuel Mesquita <mmesquita-ext@payplug.com>
2024-05-02 10:16:50 +0100

    update plugin version

--- Helper/Config.php
+++ Helper/Config.php
@@ -26,7 +26,7 @@ class Config extends AbstractHelper
     public const PAYMENT_PAGE_EMBEDDED = 'embedded';
     public const PAYMENT_PAGE_INTEGRATED = 'integrated';
 
-    public const MODULE_VERSION = '1.27.3';
+    public const MODULE_VERSION = '1.27.4';
 
     /**
      * @var WriterInterface
