2a6e89f8db2506850f3f85021536cdd937aaa24f
Benoit ALIX <benoit.alix@dnd.fr>
2024-06-26 21:12:55 +0200

    MAG-302: Update and refacto Oney view handler script

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,4 +1,9 @@
 const config = {
+    map: {
+        '*': {
+            'oneyPopin': 'Payplug_Payments/js/view/oney/popin'
+        }
+    },
     paths: {
         'payplugIntegrated': 'https://cdn.payplug.com/js/integrated-payment/v1/index'
     }
---
+++ view/frontend/web/js/view/oney/popin.js
@@ -0,0 +1,180 @@
+define([
+    'jquery',
+    'mage/url'
+], function ($, urlBuilder) {
+    'use strict';
+
+    $.widget('payplug.oneyPopin', {
+        hasChanged: true,
+        popin: '.oneyPopin',
+        triggerButton: $('.oneyCta'),
+
+        /**
+         * @private
+         */
+        _bind: function () {
+            const self = this;
+            const body = $('body');
+
+            body.on('click', '.oneyCta_button', function (event) {
+                self.togglePopin(event);
+            });
+    
+            body.on('click', '.oneyPopin_close', function () {
+                self.hidePopin();
+            });
+
+            body.on('input', '[name="qty"]', function () {
+                self._updateOneyWrapper();
+            });
+    
+            body.on('change', '.super-attribute-select', function () {
+                self._updateOneyWrapper();
+            });
+    
+            body.on('click', function (event) {
+                const clicked = $(event.target);
+                
+                if (!clicked.hasClass('oneyPopin') 
+                    && clicked.closest('.oneyPopin').length === 0
+                    && self.triggerButton.hasClass('oneyCta-open')) {
+                    self.hidePopin();
+                }
+            });
+        },
+
+        /**
+         * @private
+         */
+        _create: function () {
+            this._bind();
+        },
+
+        /**
+         * Get form data
+         * @return {Object}
+         * @private
+         */
+        _getFormData: function () {
+            let formData = {
+                form_key: $('[name="form_key"]').val()
+            };
+    
+            if ($('.oney-product').length === 0) {
+                return formData;
+            }
+    
+            const product = $('[name="product"]');
+
+            if (product.length !== 0) {
+                formData.product = product.val();
+                formData.qty = $('[name="qty"]').val();
+                let productOptions = [];
+                let configurableAttributes = $('.super-attribute-select');
+
+                if (configurableAttributes.length > 0) {
+                    configurableAttributes.each(function (index, value) {
+                        let configurableAttribute = $(value);
+
+                        productOptions.push({
+                            attribute: configurableAttribute.data('attr-name'),
+                            value: configurableAttribute.val()
+                        });
+                    });
+                }
+
+                formData.product_options = productOptions;
+            }
+    
+            return formData;
+        },
+
+        /**
+         * Update Oney wrapper
+         * @private
+         */
+        _updateOneyWrapper: function () {
+            const self = this;
+            let formData = this._getFormData();
+            formData.wrapper = true;
+    
+            $.ajax({
+                url: urlBuilder.build('payplug_payments/oney/simulation'),
+                type: 'POST',
+                data: formData,
+            }).done(function (response) {
+                if (response.success) {
+                    $('.oney-wrapper').html(response.html);
+                    self.hasChanged = true;
+                }
+            });
+        },
+
+        /**
+         * Update Oney simulation
+         * @private
+         */
+        _updateOneySimulation: function () {
+            const self = this;
+            const popin = $(this.popin);
+
+            popin
+                .addClass('loading')
+                .loader({
+                    icon: require.toUrl('images/loader-1.gif'),
+                })
+                .loader('show');
+    
+            $.ajax({
+                url: urlBuilder.build('payplug_payments/oney/simulation'),
+                type: 'POST',
+                data: this._getFormData(),
+            }).done(function (response) {
+                if (response.success) {
+                    popin.removeClass('loading').html(response.html);
+                    self.hasChanged = false;
+                }
+            });
+        },
+
+        /**
+         * Toggle popin
+         * @param {Event} event
+         */
+        togglePopin: function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+
+            if (this.triggerButton.hasClass('oneyCta-open')) {
+                this.hidePopin();
+                return;
+            }
+
+            this.showPopin();
+        },
+
+        /**
+         * Hide popin
+         */
+        hidePopin: function () {
+            const popin = $(this.popin);
+            popin.removeClass('oneyPopin-open');
+            this.triggerButton.removeClass('oneyCta-open');
+        },
+
+        /**
+         * Show popin
+         */
+        showPopin: function () {
+            const popin = $(this.popin);
+            popin.addClass('oneyPopin-open');
+            this.triggerButton.addClass('oneyCta-open');
+
+            if (this.hasChanged) {
+                this._updateOneySimulation();
+            }
+        }
+    });
+
+    return $.payplug.oneyPopin;
+});
