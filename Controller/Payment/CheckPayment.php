8134121cdd0874da2e972ef1072a3a223f0d9992
sheyne <sheyne@dnd.fr>
2024-07-11 13:31:15 +0200

    MAG-349: Add strict returns and fix typos for CheckPayment class
    changelog: added

--- Controller/Payment/CheckPayment.php
+++ Controller/Payment/CheckPayment.php
@@ -1,82 +1,71 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Payplug\Payments\Controller\Payment;
 
 use Magento\Framework\Controller\Result\Json;
 use Magento\Framework\Controller\ResultFactory;
-use Magento\Framework\Exception\PaymentException;
 use Magento\Sales\Model\Order;
-use Payplug\Exception\PayplugException;
-use Payplug\Payments\Helper\Data as Helper;
-use Payplug\Payments\Logger\Logger;
-
-use Magento\Sales\Model\OrderRepository;
-use Payplug\Payments\Exception\OrderAlreadyProcessingException;
-use Payplug\Payments\Model\Order\Payment;
-
 
 class CheckPayment extends AbstractPayment
 {
+    /**
+     * Retrieve PayPlug Standard payment url
+     *
+     * @return Json
+     */
+    public function execute(): Json
+    {
+        $response = $this->resultFactory->create(ResultFactory::TYPE_JSON);
 
+        $paymentId = $this->getRequest()->getParam("payment_id");
+        $order = $this->getLastOrder();
+        $storeId = $order->getStoreId();
 
-  /**
-   * Retrieve PayPlug Standard payment url
-   *
-   * @return Json
-   */
-  public function execute()
-  {
+        if (empty($paymentId)) {
+            throw new \Exception('Could not retrieve payment id for integrated payment');
+        }
 
-    $response = $this->resultFactory->create(ResultFactory::TYPE_JSON);
+        $payment = $this->payplugHelper->getPayment($order, $storeId);
+        if ((isset($payment->failure)) && (!empty($payment->failure)) || ($payment->is_paid === false && is_null($payment->paid_at))) {
+            $order->setStatus(Order::STATE_CANCELED);
+            $this->payplugHelper->updateOrder($order, ['status' => Order::STATE_CANCELED]);
+            $data = [
+                "error" => true,
+                'message' => __('The transaction was aborted and your card has not been charged'),
+            ];
+            $response->setData($data);
 
-    $paymentId = $this->getRequest()->getParam("payment_id");
-    $order = $this->getLastOrder();
-    $store_id = $order->getStoreId();
+            return $response;
+        }
 
-    if (empty($paymentId)) {
-      throw new \Exception('Could not retrieve payment id for integrated payment');
+        $data = ['error' => false];
+        $response->setData($data);
+
+        return $response;
     }
 
-    $payment = $this->payplugHelper->getPayment($order, $store_id);
-    if ((isset($payment->failure)) && (!empty($payment->failure)) || ($payment->is_paid === false && is_null($payment->paid_at))) {
-      $order->setStatus(Order::STATE_CANCELED);
-      $this->payplugHelper->updateOrder($order, ['status'=>Order::STATE_CANCELED]);
-      $data = [
-        "error" => true,
-        'message' => __('The transaction was aborted and your card has not been charged'),
-      ];
-      $response->setData($data);
-      return $response;
+    /**
+     * @return Order
+     *
+     * @throws \Exception
+     */
+    private function getLastOrder(): Order
+    {
+        $lastIncrementId = $this->getCheckout()->getLastRealOrderId();
+
+        if (!$lastIncrementId) {
+            throw new \Exception('Could not retrieve last order id');
+        }
+
+        $order = $this->salesOrderFactory->create();
+        $order->loadByIncrementId($lastIncrementId);
+
+        if (!$order->getId()) {
+            throw new \Exception(sprintf('Could not retrieve order with id %s', $lastIncrementId));
+        }
+
+        return $order;
     }
-
-    $data = ['error' => false];
-    $response->setData($data);
-    return $response;
-  }
-
-
-  /**
-   * Get last order
-   *
-   * @return Order
-   *
-   * @throws \Exception
-   */
-  private function getLastOrder()
-  {
-    $lastIncrementId = $this->getCheckout()->getLastRealOrderId();
-
-    if (!$lastIncrementId) {
-      throw new \Exception('Could not retrieve last order id');
-    }
-    $order = $this->salesOrderFactory->create();
-    $order->loadByIncrementId($lastIncrementId);
-
-    if (!$order->getId()) {
-      throw new \Exception(sprintf('Could not retrieve order with id %s', $lastIncrementId));
-    }
-
-    return $order;
-  }
-
 }
