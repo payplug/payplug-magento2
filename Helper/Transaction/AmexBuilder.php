1cae958a2190bcc68b363c1b23b9a7a01d89437b
sheyne <sheyne@dnd.fr>
2024-12-09 10:42:39 +0100

    v4.2.0: Fix Amex strict type
    Changelog: fixed

--- Helper/Transaction/AmexBuilder.php
+++ Helper/Transaction/AmexBuilder.php
@@ -6,7 +6,7 @@ namespace Payplug\Payments\Helper\Transaction;
 
 use Magento\Payment\Gateway\Data\OrderAdapterInterface;
 use Magento\Payment\Model\InfoInterface;
-use Magento\Quote\Model\Quote;
+use Magento\Quote\Api\Data\CartInterface;
 use Magento\Sales\Api\Data\OrderInterface;
 
 class AmexBuilder extends AbstractBuilder
@@ -14,7 +14,7 @@ class AmexBuilder extends AbstractBuilder
     /**
      * @inheritdoc
      */
-    public function buildPaymentData(OrderInterface|OrderAdapterInterface $order, InfoInterface $payment, Quote $quote): array
+    public function buildPaymentData(OrderInterface|OrderAdapterInterface $order, InfoInterface $payment, CartInterface $quote): array
     {
         $paymentData = parent::buildPaymentData($order, $payment, $quote);
         $paymentData['payment_method'] = 'american_express';
