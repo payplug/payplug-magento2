7f84e5b906e833be8f4315794ec44c8a3e3cedce
sheyne <sheyne@dnd.fr>
2025-04-22 13:33:36 +0200

    MAG-401: Clean and refacto the typing and functions for ApplePay.php and Button.php
    Changelog: Added

--- Helper/ApplePay.php
+++ Helper/ApplePay.php
@@ -1,106 +1,73 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Payplug\Payments\Helper;
 
+use Magento\Framework\App\Helper\Context;
+use Payplug\Payments\Gateway\Config\ApplePay as ApplePayConfig;
 use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Payment\Helper\Data;
 use Magento\Store\Model\ScopeInterface;
-use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Store\Model\StoreManagerInterface;
 
 class ApplePay extends AbstractHelper
 {
     /**
-     * @var StoreManagerInterface
+     * Constants to display Apple Pay on several pages
      */
-    private $storeManager;
+    public const ACTIVE_APPLE_PAY_PAGE = 'active';
+    public const CART_APPLE_PAY_PAGE = 'show_on_cart';
+    public const CHECKOUT_APPLE_PAY_PAGE = 'show_on_checkout';
 
-    /**
-     * @var \Magento\Payment\Helper\Data
-     */
-    private $paymentHelper;
+    private string $applePayMethod;
 
-    /**
-     * @var string
-     */
-    private $applePayMethod;
-
-    /**
-     * @param StoreManagerInterface        $storeManager
-     * @param ScopeConfigInterface         $scopeConfig
-     * @param \Magento\Payment\Helper\Data $paymentHelper
-     */
     public function __construct(
-        StoreManagerInterface $storeManager,
-        ScopeConfigInterface $scopeConfig,
-        \Magento\Payment\Helper\Data $paymentHelper,
+        Context $context,
+        private StoreManagerInterface $storeManager,
+        private Data $paymentHelper,
     ) {
-        $this->storeManager = $storeManager;
-        $this->paymentHelper = $paymentHelper;
-        $this->scopeConfig = $scopeConfig;
+        parent::__construct($context);
     }
-    
+
     /**
      * Check if Apple Pay method can display ion specific page
-     *
-     * @return string
      */
-    private function canDisplayApplePayOnPage($page): string
+    private function canDisplayApplePayOnPage(string $page): bool
     {
         $storeId = $this->storeManager->getStore()->getId();
-
         $applePayPaymentMethod = $this->getApplePayMethod();
 
         if ($applePayPaymentMethod === '') {
             return false;
         }
 
-        return $this->scopeConfig->getValue(
+        return (bool)$this->scopeConfig->getValue(
             'payment/' . $applePayPaymentMethod . '/' . $page,
             ScopeInterface::SCOPE_STORE,
             $storeId
         );
     }
 
-    /**
-     * Check Apple Pay availability
-     *
-     * @return bool
-     */
     public function canDisplayApplePay(): bool
     {
-        return $this->canDisplayApplePayOnPage('active');
+        return $this->canDisplayApplePayOnPage(ApplePay::ACTIVE_APPLE_PAY_PAGE);
     }
 
-    /**
-     * Check Apple Pay availability on cart page
-     *
-     * @return bool
-     */
     public function canDisplayApplePayOnCart(): bool
     {
-        return $this->canDisplayApplePayOnPage('show_on_cart');
+        return $this->canDisplayApplePayOnPage(ApplePay::CART_APPLE_PAY_PAGE);
     }
 
-    /**
-     * Check Apple Pay availability on checkout page
-     *
-     * @return bool
-     */
     public function canDisplayApplePayOncheckout(): bool
     {
-        return $this->canDisplayApplePayOnPage('show_on_checkout');
+        return $this->canDisplayApplePayOnPage(ApplePay::CHECKOUT_APPLE_PAY_PAGE);
     }
 
-    /**
-     * Get available Apple Pay method
-     *
-     * @return string
-     */
-    private function getApplePayMethod()
+    private function getApplePayMethod(): string
     {
-        if ($this->applePayMethod === null) {
-            $applePayMethod = \Payplug\Payments\Gateway\Config\ApplePay::METHOD_CODE;
-
+        if (!isset($this->applePayMethod)) {
+            $applePayMethod = ApplePayConfig::METHOD_CODE;
             if ($this->paymentHelper->getMethodInstance($applePayMethod)->isAvailable()) {
                 $this->applePayMethod = $applePayMethod;
             }
