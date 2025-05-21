3c3986bbe3024140a42b4b6d5d2a859133a35c98
Frédéric TISSOT <contact@espritdev.fr>
2025-05-21 20:08:07 +0200

    MAG-403: ApplePay PDP - Refactoring create quote + add compatibility with Products having custom options + Configurable product type

--- Controller/ApplePay/CreateApplePayQuote.php
+++ Controller/ApplePay/CreateApplePayQuote.php
@@ -4,90 +4,61 @@ declare(strict_types=1);
 
 namespace Payplug\Payments\Controller\ApplePay;
 
-use Exception;
+use Magento\Checkout\Controller\Cart\Add as AddToCartController;
+use Magento\Checkout\Model\SessionFactory as CheckoutSessionFactory;
 use Magento\Framework\App\Action\HttpPostActionInterface;
-use Magento\Framework\App\RequestInterface;
+use Magento\Framework\App\Response\Http as HttpResponse;
 use Magento\Framework\Controller\Result\Json;
 use Magento\Framework\Controller\Result\JsonFactory;
-use Magento\Framework\DataObject;
-use Magento\Framework\Exception\AlreadyExistsException;
+use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Quote\Api\CartRepositoryInterface;
-use Magento\Checkout\Model\Session as CheckoutSession;
-use Magento\Catalog\Api\ProductRepositoryInterface;
-use Magento\Framework\Data\Form\FormKey\Validator;
-use Magento\Framework\Exception\LocalizedException;
 
 class CreateApplePayQuote implements HttpPostActionInterface
 {
     public function __construct(
         private readonly JsonFactory $resultJsonFactory,
-        private readonly CheckoutSession $checkoutSession,
-        private readonly ProductRepositoryInterface $productRepository,
-        private readonly Validator $formKeyValidator,
-        private readonly RequestInterface $request,
+        private readonly CheckoutSessionFactory $checkoutSessionFactory,
+        private readonly AddToCartController $addToCartController,
         private readonly CartRepositoryInterface $cartRepository
     ) {
     }
 
     /**
      * @throws NoSuchEntityException
-     * @throws AlreadyExistsException
      * @throws LocalizedException
      */
     public function execute(): Json
     {
-        $result = $this->resultJsonFactory->create();
+        $checkoutSession = $this->checkoutSessionFactory->create();
+        $currentQuote = $checkoutSession->getQuote();
+        $currentQuote->removeAllItems();
 
-        if (!$this->formKeyValidator->validate($this->request)) {
-            return $result->setData(['error' => true, 'message' => 'Invalid form key.']);
-        }
+        $jsonResult = $this->resultJsonFactory->create();
+        $addToCartResponse = $this->addToCartController->execute();
 
-        $params = $this->request->getParams();
-        $productId = (int)($params['product'] ?? 0);
-        $qty = (float)($params['qty'] ?? 1);
-
-        // TODO bundle product parameters
-        // TODO grouped product parameters
-        // TODO configurable parameters ?
-        // TODO Custom options ?
-
-        try {
-            $quote = $this->checkoutSession->getQuote();
-            $quote->removeAllItems();
-
-            $product = $this->productRepository->getById($productId);
-            $buyRequest = new DataObject(['qty' => $qty]);
-            $resultAdd = $quote->addProduct($product, $buyRequest);
-
-            if (is_string($resultAdd)) {
-                throw new LocalizedException(__($resultAdd));
-            }
-
-            $this->cartRepository->save($quote);
+        if ($addToCartResponse instanceof HttpResponse) {
+            $checkoutSession = $this->checkoutSessionFactory->create();
+            $quote = $checkoutSession->getQuote();
 
             if ($quote->isVirtual() === false) {
                 $shippingAddress = $quote->getShippingAddress();
                 $shippingAddress->setShippingMethod('');
                 $shippingAddress->setCollectShippingRates(true)->collectShippingRates();
+
+                $quote->setTotalsCollectedFlag(false);
+                $quote->collectTotals();
+                $this->cartRepository->save($quote);
             }
 
-            $quote->setTotalsCollectedFlag(false);
-            $quote->collectTotals();
-
-            $this->cartRepository->save($quote);
-
-            return $result->setData([
+            return $jsonResult->setData([
                 'success' => true,
                 'message' => 'Quote created',
                 'base_amount' => (float)$quote->getGrandTotal(),
                 'is_virtual' => $quote->isVirtual()
             ]);
-        } catch (Exception $e) {
-            return $result->setData([
-                'error' => true,
-                'message' => $e->getMessage(),
-            ]);
+        } else {
+            return $jsonResult->setData(['error' => true, 'message' => __('An error occurred while adding product.')]);
         }
     }
 }
