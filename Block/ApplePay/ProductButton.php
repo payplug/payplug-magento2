0c40e1722393f3b6decc20d7879cbbb749856ef3
Frédéric TISSOT <contact@espritdev.fr>
2025-06-11 18:25:09 +0200

    MAG-403: ApplePay - Rework getCurrentProduct

--- Block/ApplePay/ProductButton.php
+++ Block/ApplePay/ProductButton.php
@@ -7,12 +7,10 @@ use Magento\Framework\Exception\NoSuchEntityException;
 
 class ProductButton extends AbstractButton
 {
-    private ?ProductInterface $currentProduct = null;
+    private array $productCache = [];
 
     public function _toHtml(): string
     {
-        $this->setCurrentProduct();
-
         if ($this->getRequest()->getFullActionName() === 'catalog_product_view'
             && $this->applePayHelper->canDisplayApplePay()
             && $this->applePayHelper->canDisplayApplePayOnProduct()
@@ -24,21 +22,25 @@ class ProductButton extends AbstractButton
         return '';
     }
 
-    private function setCurrentProduct(): void
-    {
-        $productId = $this->getRequest()->getParam('id');
-
-        if ($productId) {
-            try {
-                $storeId = $this->storeManager->getStore()->getId();
-                $this->currentProduct = $this->productRepository->getById($productId, false, $storeId);
-            } catch (NoSuchEntityException) {
-            }
-        }
-    }
-
     public function getCurrentProduct(): ?ProductInterface
     {
-        return $this->currentProduct;
+        $productId = (int) $this->getRequest()->getParam('id');
+
+        if (!$productId) {
+            return null;
+        }
+
+        if (array_key_exists($productId, $this->productCache)) {
+            return $this->productCache[$productId];
+        }
+
+        try {
+            $storeId = (int) $this->storeManager->getStore()->getId();
+            $product = $this->productRepository->getById($productId, false, $storeId);
+        } catch (NoSuchEntityException) {
+            $product = null;
+        }
+
+        return $this->productCache[$productId] = $product;
     }
 }
