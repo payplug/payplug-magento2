03b87efc41f5aa6b91b98f3e52c3c555b5f38887
Frédéric TISSOT <contact@espritdev.fr>
2025-06-30 16:57:47 +0200

    MAG-378: Remove unused method

--- Controller/Adminhtml/Config/Oauth2Logout.php
+++ Controller/Adminhtml/Config/Oauth2Logout.php
@@ -5,13 +5,10 @@ declare(strict_types=1);
 namespace Payplug\Payments\Controller\Adminhtml\Config;
 
 use Magento\Framework\App\Action\HttpGetActionInterface;
-use Magento\Framework\App\Config\ScopeConfigInterface;
-use Magento\Framework\App\Config\Storage\WriterInterface as ConfigWriterInterface;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\Controller\Result\RedirectFactory;
 use Magento\Framework\Controller\ResultInterface;
 use Magento\Framework\Message\ManagerInterface as MessageManagerInterface;
-use Magento\Store\Model\ScopeInterface as StoreScopeInterface;
 use Payplug\Payments\Helper\Config as ConfigHelper;
 
 class Oauth2Logout implements HttpGetActionInterface
@@ -20,7 +17,6 @@ class Oauth2Logout implements HttpGetActionInterface
         private readonly RedirectFactory $redirectFactory,
         private readonly RequestInterface $request,
         private readonly MessageManagerInterface $messageManager,
-        private readonly ConfigWriterInterface $configWriter,
         private readonly ConfigHelper $configHelper
     ) {
     }
@@ -35,17 +31,6 @@ class Oauth2Logout implements HttpGetActionInterface
         return $this->redirectFactory->create()->setPath(
             'adminhtml/system_config/edit',
             ['section' => 'payplug_payments', 'website' => $this->getWebsiteId()]
-        );
-    }
-
-    private function deleteConfig(string $path): void
-    {
-        $websiteId = $this->getWebsiteId();
-
-        $this->configWriter->delete(
-            $path,
-            $websiteId ? StoreScopeInterface::SCOPE_WEBSITES : ScopeConfigInterface::SCOPE_TYPE_DEFAULT,
-            $websiteId ?: 0
         );
     }
 
