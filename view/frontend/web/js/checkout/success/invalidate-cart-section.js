2db558660587d140752dbfa36c3958936ab591d4
Frédéric TISSOT <contact@espritdev.fr>
2025-08-06 15:58:36 +0200

    MAG-465: Fix invalidate cart section script / conflict with hyva theme

---
+++ view/frontend/web/js/checkout/success/invalidate-cart-section.js
@@ -0,0 +1,10 @@
+define([
+    'Magento_Customer/js/customer-data'
+], function (customerData) {
+    'use strict';
+
+    return function () {
+        customerData.invalidate(['cart']);
+        customerData.reload(['cart'], true);
+    };
+});
