5c32565a801141ef9418928500a94374fda667a1
Frédéric TISSOT <contact@espritdev.fr>
2025-09-01 14:23:24 +0200

    MAG-465: Fixes

--- ViewModel/CheckoutSuccessViewModel.php
+++ ViewModel/CheckoutSuccessViewModel.php
@@ -2,23 +2,21 @@
 
 namespace Payplug\Payments\ViewModel;
 
+use Magento\Checkout\Model\Session as CheckoutSession;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
-use Magento\Sales\Api\Data\OrderInterfaceFactory;
 use Payplug\Payments\Helper\Data as PayplugDataHelper;
 
 class CheckoutSuccessViewModel implements ArgumentInterface
 {
     public function __construct(
         private readonly PayplugDataHelper $payplugDataHelper,
-        private readonly OrderInterfaceFactory $orderFactory
+        private readonly CheckoutSession $checkoutSession
     ) {
     }
 
-    public function needToInvalidateCustomerCartSection(string $orderIncrementId): bool
+    public function needToInvalidateCustomerCartSection(): bool
     {
-        $order = $this->orderFactory->create();
-        $order->loadByIncrementId($orderIncrementId);
-
+        $order = $this->checkoutSession->getLastRealOrder();
         $payment = $order->getPayment();
 
         if ($payment && $this->payplugDataHelper->isCodePayplugPaymentWithRedirect($payment->getMethod()) === true) {
