91c5035aec1abc491ad737b59f4f6205818b0ec0
Frédéric TISSOT <contact@espritdev.fr>
2025-12-01 16:36:42 +0100

    MAG-590: Prevent transactional order confirmation email on order creation

--- Gateway/Config/PayplugPayment.php
+++ Gateway/Config/PayplugPayment.php
@@ -12,11 +12,6 @@ class PayplugPayment extends Config
 {
     public function getValue($field, $storeId = null)
     {
-        if ($field === 'order_place_redirect_url') {
-            // Prevent order email sending when placing the order
-            return true;
-        }
-
         if ($field === 'payment_action'
             && parent::getValue($field, $storeId) === MethodInterface::ACTION_AUTHORIZE_CAPTURE
             && $this->getValue('invoice_on_payment', $storeId)
