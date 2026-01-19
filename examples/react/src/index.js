9998c7c904f1d87b3f6a686173c7d96451673f80
Benoit Alix <benoit.alix@dnd.fr>
2026-01-19 14:44:17 +0100

    MAG-617: Update React components declaration to avoid PHP CS errors

--- examples/react/src/index.js
+++ examples/react/src/index.js
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <App></App>
     </ApolloProvider>
   </React.StrictMode>
 );
