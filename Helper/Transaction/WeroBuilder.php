ee5ea018f85cf078956a46959ed3a0f7d2edd31a
Frédéric TISSOT <contact@espritdev.fr>
2026-01-21 17:15:11 +0100

    MAG-603: Add Bizum and Wero payment methods

---
+++ Helper/Transaction/WeroBuilder.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Payplug - https://www.payplug.com/
+ * Copyright © Payplug. All rights reserved.
+ * See LICENSE for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Payplug\Payments\Helper\Transaction;
+
+use Magento\Payment\Model\InfoInterface;
+use Magento\Quote\Api\Data\CartInterface;
+
+class WeroBuilder extends ApmBuilder
+{
+    /**
+     * @inheritdoc
+     */
+    public function buildPaymentData($order, InfoInterface $payment, CartInterface $quote): array
+    {
+        $paymentData = parent::buildPaymentData($order, $payment, $quote);
+        $paymentData['payment_method'] = 'wero';
+
+        return $paymentData;
+    }
+}
