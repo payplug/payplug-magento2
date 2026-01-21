7b58a1e70589b5911c6cbd51dc4ef3a40753ec3b
Frédéric TISSOT <contact@espritdev.fr>
2026-01-21 16:19:39 +0100

    MAG-618: Add quote retrieval logic to TransactionDataBuilder with enhanced error handling

--- Gateway/Request/Payment/TransactionDataBuilder.php
+++ Gateway/Request/Payment/TransactionDataBuilder.php
@@ -2,44 +2,54 @@
 
 namespace Payplug\Payments\Gateway\Request\Payment;
 
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Payment\Gateway\Request\BuilderInterface;
+use Magento\Quote\Api\CartRepositoryInterface;
+use Magento\Sales\Model\Order\Payment;
 use Payplug\Payments\Gateway\Helper\SubjectReader;
 use Payplug\Payments\Helper\Transaction\AbstractBuilder;
+use Payplug\Payments\Logger\Logger as PayplugLogger;
 
 class TransactionDataBuilder implements BuilderInterface
 {
     /**
-     * @var SubjectReader
-     */
-    private $subjectReader;
-
-    /**
-     * @var AbstractBuilder
-     */
-    private $builder;
-    
-    /**
-     * @param SubjectReader   $subjectReader
+     * @param SubjectReader $subjectReader
      * @param AbstractBuilder $builder
+     * @param CartRepositoryInterface $cartRepository
+     * @param PayplugLogger $payplugLogger
      */
     public function __construct(
-        SubjectReader $subjectReader,
-        AbstractBuilder $builder
+        private readonly SubjectReader $subjectReader,
+        private readonly AbstractBuilder $builder,
+        private readonly CartRepositoryInterface $cartRepository,
+        private readonly PayplugLogger $payplugLogger
     ) {
-        $this->subjectReader = $subjectReader;
-        $this->builder = $builder;
     }
 
     /**
      * @inheritdoc
      */
-    public function build(array $buildSubject)
+    public function build(array $buildSubject): array
     {
         $paymentDO = $this->subjectReader->readPayment($buildSubject);
 
         $order = $paymentDO->getOrder();
         $payment = $paymentDO->getPayment();
-        $quote = $this->subjectReader->getQuote();
+        $quote = null;
+
+        if ($payment instanceof Payment) {
+            $quoteId = $payment->getOrder()->getQuoteId();
+
+            try {
+                $quote = $this->cartRepository->get($quoteId);
+            } catch (NoSuchEntityException $e) {
+                $this->payplugLogger->error($e->getMessage());
+            }
+        }
+
+        if ($quote === null) {
+            $quote = $this->subjectReader->getQuote();
+        }
 
         return $this->builder->buildTransaction($order, $payment, $quote);
     }
