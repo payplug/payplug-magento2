7971c137dcf05b5412909af9747397571161cfa9
Frédéric TISSOT <contact@espritdev.fr>
2026-01-26 12:25:14 +0100

    MAG-615: Add Payplug Copyright comments

--- view/frontend/web/js/view/payment/payplug_payments_apple_pay.js
+++ view/frontend/web/js/view/payment/payplug_payments_apple_pay.js
@@ -1,3 +1,9 @@
+/**
+ * Payplug - https://www.payplug.com/
+ * Copyright © Payplug. All rights reserved.
+ * See LICENSE for license details.
+ */
+
 define([
     'uiComponent',
     'Magento_Checkout/js/model/payment/renderer-list'
