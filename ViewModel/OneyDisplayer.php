7c7e6621e169a9ad4813a3083bf6012c63542364
Benoit Alix <benoit.alix@dnd.fr>
2026-01-26 18:13:38 +0100

    MAG-626: Add Hyva support for Payplug custom theme devs

---
+++ ViewModel/OneyDisplayer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Payplug - https://www.payplug.com/
+ * Copyright © Payplug. All rights reserved.
+ * See LICENSE for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Hyva\CheckoutPayplug\ViewModel;
+
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+use Payplug\Payments\Helper\Oney;
+
+class OneyDisplayer implements ArgumentInterface
+{
+    public function __construct(
+        private Oney $oney
+    ) {
+    }
+
+    public function canDisplayOney(): bool
+    {
+        return $this->oney->canDisplayOney();
+    }
+}
---
+++ ViewModel/PayplugIcons.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Payplug - https://www.payplug.com/
+ * Copyright © Payplug. All rights reserved.
+ * See LICENSE for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Payplug\Payments\ViewModel;
+
+use Hyva\Theme\ViewModel\SvgIcons;
+
+class PayplugIcons extends SvgIcons
+{
+
+}
