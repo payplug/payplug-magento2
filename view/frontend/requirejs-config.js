68a159f60a68f9723643aba547521f556aec216a
Frédéric TISSOT <contact@espritdev.fr>
2026-02-04 09:44:00 +0100

    MAG-565: Support for Payplug Payments QA extension

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -9,8 +9,5 @@ const config = {
         '*': {
             'oneyPopin': 'Payplug_Payments/js/view/oney/popin'
         }
-    },
-    paths: {
-        'payplugIntegrated': 'https://cdn.payplug.com/js/integrated-payment/v1/index'
     }
 };
